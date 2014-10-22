5020ed4ed728d7730333915a5cdd418429ab5638
Kyle Doherty <kyledoherty22@gmail.com>
2014-10-22 18:14:03 -0400

    Refactor worflow do block to dry it up

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,38 +2,26 @@ class Project < ActiveRecord::Base
   include Workflow
 
   workflow do
-    state :active do
-      #repeating myself for each one of these, need to dry up
-      event :active_to_queued, transitions_to: :queued
-      event :active_to_on_hold, transitions_to: :on_hold
-      event :active_to_completed, transitions_to: :completed
-    end
-
-    state :queued do
-      event :to_active, transitions_to: :active
-      event :queued_to_on_hold, transitions_to: :on_hold
-      event :queued_to_completed, transitions_to: :completed
-    end
-
-    state :on_hold do
-      event :to_active, transitions_to: :active
-      event :on_hold_to_queued, transitions_to: :queued
-      event :on_hold_to_completed, transitions_to: :completed
-    end
-
-    state :completed do
-      event :to_active, transitions_to: :active
-      event :completed_to_on_hold, transitions_to: :on_hold
-      event :completed_to_queued, transitions_to: :queued
+    [:active, :queued, :on_hold, :completed].each do |state_name|
+      state state_name do
+        event :to_queued, transitions_to: :queued
+        event :to_on_hold, transitions_to: :on_hold
+        event :to_completed, transitions_to: :completed
+        event :to_active, transitions_to: :active
+      end
     end
   end
 
-  #need method that anytime a project status is set to active this method fires
   def to_active
     puts "Email the Lead Analyst that the client changed this project to active!"
   end
 
+  def to_completed
+    puts "Email the Lead Analyst that the client changed this project to completed!"
+  end
 
-
+  def to_on_hold
+    puts "lol"
+  end
 
 end
