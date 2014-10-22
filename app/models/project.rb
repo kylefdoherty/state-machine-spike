2570c91050ba7b9a67ed84a0a8cdc32b1c7696bd
Kyle Doherty <kyledoherty22@gmail.com>
2014-10-22 17:04:14 -0400

    Refactor method to set state to active

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,27 +10,30 @@ class Project < ActiveRecord::Base
     end
 
     state :queued do
-      event :queued_to_active, transitions_to: :active
+      event :to_active, transitions_to: :active
       event :queued_to_on_hold, transitions_to: :on_hold
       event :queued_to_completed, transitions_to: :completed
     end
 
     state :on_hold do
-      event :on_hold_to_active, transitions_to: :active
+      event :to_active, transitions_to: :active
       event :on_hold_to_queued, transitions_to: :queued
       event :on_hold_to_completed, transitions_to: :completed
     end
 
     state :completed do
-      event :completed_to_active, transitions_to: :active
+      event :to_active, transitions_to: :active
       event :completed_to_on_hold, transitions_to: :on_hold
       event :completed_to_queued, transitions_to: :queued
     end
   end
 
   #need method that anytime a project status is set to active this method fires
-  def queued_to_active
+  def to_active
     puts "Email the Lead Analyst that the client changed this project to active!"
   end
+
+
+
 
 end
